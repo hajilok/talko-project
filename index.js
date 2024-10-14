c85883e5127e0617807a727f02e768214b95c79a
Imam Wahyu Widodo <120608486+hajilok@users.noreply.github.com>
2024-10-15 06:08:53 +0700

    Add Files

---
+++ index.js
@@ -0,0 +1,41 @@
+import fs from 'fs/promises'
+import figlet from 'figlet';
+import chalk from 'chalk';
+import swapRitzu from './ritzu/swap.js';
+
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+// Membuat banner dengan figlet
+const displayBanner = () => {
+    console.log(chalk.cyan(figlet.textSync('Makmum Airdrop', {
+      font: 'Slant',
+      horizontalLayout: 'default',
+      verticalLayout: 'default',
+      width: 80,
+      whitespaceBreak: true
+    })));
+  };
+  
+
+const main = async () => {
+    displayBanner();
+    const wallet = (await fs.readFile('wallet.txt', 'utf-8'))
+        .replace(/\r/g, "")
+        .split('\n')
+        .filter(Boolean);
+
+    while (true) {
+        for (let i = 0; i < wallet.length; i++) {
+            const walletAddress = wallet[i];
+            const formattedPrivateKey = walletAddress.startsWith('0x') ? walletAddress : '0x' + walletAddress;
+            const ritzu = await swapRitzu(formattedPrivateKey);
+            console.log(chalk.bgGreen(`Succesfully Wraped And Unwraped Eth To Weth In Ritzu : https://taikoscan.io/tx/${ritzu}`));
+        }
+
+        // Delay 1 menit sebelum loop dijalankan kembali
+        console.log(chalk.blue('Menunggu 10 menit sebelum Wraped berikutnya...'));
+        await delay(600000); // 60000 ms = 1 menit
+    }
+}
+
+main();
---
+++ ritzu/swap.js
@@ -0,0 +1,73 @@
+import Web3 from "web3";
+import axios from "axios";
+
+const swapRitzu = async (priv) => {
+    const web3 = new Web3('https://rpc.taiko.xyz')
+    const account = web3.eth.accounts.privateKeyToAccount(priv)
+    web3.eth.accounts.wallet.add(account)
+    console.log(`Account ${account.address} To do Wrapped ETH`)
+    const wrapedEth = async (getkey) => {
+        const BaseContract = '0xa51894664a773981c6c112c43ce576f315d5b1b6'
+        const getABi = await axios.get(`https://api.taikoscan.io/api?module=contract&action=getabi&address=${BaseContract}`)
+        const abi = JSON.parse(getABi.data.result)
+        const contract = new web3.eth.Contract(abi,  BaseContract)
+        const amount = web3.utils.toWei('0.001', 'ether')
+        const data = contract.methods.deposit().encodeABI()
+        const gasEstimate = await web3.eth.estimateGas({
+            from: account.address,
+            to: BaseContract,
+            data: data,
+            value: amount
+        });
+        const tx = {
+            from:  account.address,
+            to: BaseContract,
+            data: data,
+            value: amount,
+            gas: gasEstimate,
+            gasPrice: 100000000
+        }
+        const receipt = await web3.eth.accounts.signTransaction(tx, getkey)
+        const  txHash = await web3.eth.sendSignedTransaction(receipt.rawTransaction)
+        return {
+            txHash: txHash,
+            amount: amount
+        }
+    }
+
+    const getWraped = await wrapedEth(priv)
+    if  (!getWraped.txHash) {
+        return `wraped eth error, check your gass fee and  priv key`
+    }
+
+    const unwraped = async (getskey) => {
+        const BaseContract = '0xa51894664a773981c6c112c43ce576f315d5b1b6'
+        const getABi = await axios.get(`https://api.taikoscan.io/api?module=contract&action=getabi&address=${BaseContract}`)
+        const abi = JSON.parse(getABi.data.result)
+        const amount = 1000000000000000
+        const contract = new web3.eth.Contract(abi, BaseContract)
+        const data = contract.methods.withdraw(amount).encodeABI()
+        const gasEstimate = await web3.eth.estimateGas({
+            from: account.address,
+            to: BaseContract,
+            data: data,
+        });
+        const tx = {
+            from:  account.address,
+            to: BaseContract,
+            data: data,
+            gas: gasEstimate,
+            gasPrice: 100000000
+        }
+        const receipt = await web3.eth.accounts.signTransaction(tx, getskey)
+        const  txHash = await web3.eth.sendSignedTransaction(receipt.rawTransaction)
+        return txHash
+    }
+
+    const getData = await unwraped(priv)
+    return getData.logs[0].transactionHash
+
+
+}
+
+export default swapRitzu
