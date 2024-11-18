2d75c023c268f6abace40cc56104410f2597dcb5
Imam Wahyu Widodo <120608486+hajilok@users.noreply.github.com>
2024-11-18 07:55:18 +0700

    Update Vote.js

--- RubyScore/Vote.js
+++ RubyScore/Vote.js
@@ -25,7 +25,7 @@ const voteRubyScore = async (privkey) => {
       value: web3.utils.toWei('0', 'ether'),
       data: data,
       gas: gasEstimate,
-      gasPrice: 200000000
+      gasPrice: 160000000
    }
 
    const receipt = await web3.eth.accounts.signTransaction(tx, privkey)
